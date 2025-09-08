537289142441bdedfcf3c25ead6744ca80afc2c9
naja moe <najamoe@outlook.dk>
2025-09-08 13:26:21 +0200

    ArticleDatabase created

---
+++ api/Models/Article.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace api.Models
+{
+    public class Article
+    {
+        public int Id { get; set; }
+        public required string Author { get; set; }
+        public required string Title { get; set; }
+        public required string Content { get; set; }
+
+    }
+}
