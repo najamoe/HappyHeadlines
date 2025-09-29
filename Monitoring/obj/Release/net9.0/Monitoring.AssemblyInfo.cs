e8830f9dc42fa1d1fe284eb80c9e3e06903e4bbe
naja moe <najamoe@outlook.dk>
2025-09-29 13:34:27 +0200

    misc.

--- Monitoring/obj/Release/net9.0/Monitoring.AssemblyInfo.cs
+++ Monitoring/obj/Release/net9.0/Monitoring.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Monitoring")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68756d8d1405d7d1d5214ad331556787b042308e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c0287e01973e78c573d6d43afe5aaf180e7291c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Monitoring")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Monitoring")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
