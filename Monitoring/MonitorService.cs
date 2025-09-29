8c0287e01973e78c573d6d43afe5aaf180e7291c
naja moe <najamoe@outlook.dk>
2025-09-29 13:26:15 +0200

    created the monitoring service

--- Monitoring/MonitorService.cs
+++ Monitoring/MonitorService.cs
@@ -1,7 +1,18 @@
-﻿namespace Monitoring
-{
-    public class MonitorService
-    {
+﻿using Serilog;
 
+namespace Monitoring
+{
+    public static class MonitorService
+    {
+        public static ILogger Log => Serilog.Log.Logger;
+
+        static MonitorService()
+            {
+            Serilog.Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Verbose()
+                .WriteTo.Console()
+                .WriteTo.Seq("http://localhost:5341")
+                .CreateLogger();
+        }
     }
 }
