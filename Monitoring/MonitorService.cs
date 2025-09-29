d9476b2d60502a5df039b5124e5be9a79fcf58d1
naja moe <najamoe@outlook.dk>
2025-09-29 16:10:45 +0200

    added activity tracer

--- Monitoring/MonitorService.cs
+++ Monitoring/MonitorService.cs
@@ -1,14 +1,33 @@
-﻿using Serilog;
+﻿using OpenTelemetry;
+using OpenTelemetry.Trace;
+using OpenTelemetry.Resources;
+using Serilog;
 using Serilog.Core;
+using System.Diagnostics;
+using System.Reflection;
 
 namespace Monitoring
 {
     public static class MonitorService
     {
+
+        public static readonly string ServiceName = Assembly.GetCallingAssembly().GetName().Name ?? "UnknownService";
+        public static TracerProvider TracerProvider;
+        public static ActivitySource ActivitySource = new ActivitySource(ServiceName);
+
         public static ILogger Log => Serilog.Log.Logger;
 
         static MonitorService()
             {
+            //Open Telemetry
+            TracerProvider = Sdk.CreateTracerProviderBuilder()
+                .AddConsoleExporter()
+                .AddSource(ActivitySource.Name)
+                .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(ServiceName))
+                .Build();
+
+
+            //Serilog
             Serilog.Log.Logger = new LoggerConfiguration()
                 .MinimumLevel.Verbose()
                 .WriteTo.Console()
