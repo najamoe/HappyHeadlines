e16998858e61d21ca14876871e78ae784ffa5f2f
naja moe <najamoe@outlook.dk>
2025-09-30 18:53:16 +0200

    added controller

---
+++ PublisherService/Controllers/PublisherController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PublisherService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PublisherController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
