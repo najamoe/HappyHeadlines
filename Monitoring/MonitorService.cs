c65bdcbe5e36f1f2e217fd86416458ec2f7872b1
naja moe <najamoe@outlook.dk>
2025-09-30 10:47:11 +0200

    added zipkin exporter

--- Monitoring/MonitorService.cs
+++ Monitoring/MonitorService.cs
@@ -1,19 +1,21 @@
 ﻿using OpenTelemetry;
 using OpenTelemetry.Trace;
 using OpenTelemetry.Resources;
+using OpenTelemetry.Exporter;
 using Serilog;
 using Serilog.Core;
 using System.Diagnostics;
 using System.Reflection;
+using System.Net;
 
 namespace Monitoring
 {
     public static class MonitorService
     {
 
-        public static readonly string ServiceName = Assembly.GetCallingAssembly().GetName().Name ?? "UnknownService";
+        public static readonly string ServiceName = Assembly.GetCallingAssembly().GetName().Name ?? "Unknown";
         public static TracerProvider TracerProvider;
-        public static ActivitySource ActivitySource = new ActivitySource(ServiceName);
+        public static ActivitySource ActivitySource = new ActivitySource(ServiceName, "1.0.0");
 
         public static ILogger Log => Serilog.Log.Logger;
 
@@ -22,6 +24,10 @@ namespace Monitoring
             //Open Telemetry
             TracerProvider = Sdk.CreateTracerProviderBuilder()
                 .AddConsoleExporter()
+                .AddZipkinExporter(config =>
+                {
+                    config.Endpoint = new Uri("http://zipkin:9411/api/v2/spans");
+                })
                 .AddSource(ActivitySource.Name)
                 .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(ServiceName))
                 .Build();
