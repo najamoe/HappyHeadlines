fa55264299655ad4f37c8c79c254700a2bbb6b8d
naja moe <najamoe@outlook.dk>
2025-10-02 10:37:41 +0200

    added rabbitMQ connection - queue name "SubscriberQueue"

---
+++ SubscriberService/Infrastructure/RabbitMQConnection.cs
@@ -0,0 +1,45 @@
+﻿using RabbitMQ.Client;
+using System.Threading.Tasks;
+
+namespace SubscriberService.Infrastructure
+{
+    public class RabbitMQConnection : IDisposable
+    {
+        private readonly IConnection _connection;
+        private readonly IModel _channel; // Note: IModel is the correct type
+
+        private const string QueueName = "SubscriberQueue";
+
+        public IModel Channel => _channel;
+
+        public RabbitMQConnection(string hostName = "rabbitmq", string user = "guest", string pass = "guest", string vhost = "/")
+        {
+            var factory = new ConnectionFactory
+            {
+                HostName = hostName,
+                UserName = user,
+                Password = pass,
+                VirtualHost = vhost,
+                Port = 5672
+            };
+
+            // Create connection and channel
+            _connection = factory.CreateConnection();
+            _channel = _connection.CreateModel();
+
+            // Declare queue
+            _channel.QueueDeclare(
+                queue: QueueName,
+                durable: true,
+                exclusive: false,
+                autoDelete: false
+            );
+        }
+
+        public void Dispose()
+        {
+            _channel?.Dispose();
+            _connection?.Dispose();
+        }
+    }
+}
