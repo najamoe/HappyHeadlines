ef4c9f10e166bd1b61b846001caeab05c40a03d0
naja moe <najamoe@outlook.dk>
2025-10-02 10:17:24 +0200

    added model

---
+++ SubscriberService/Models/Subscriber.cs
@@ -0,0 +1,11 @@
+﻿namespace SubscriberService.Models
+{
+    public class Subscriber
+    {
+
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+        public DateTime SubscribedOn { get; set; }
+    }
+}
