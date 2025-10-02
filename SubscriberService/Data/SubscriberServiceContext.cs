78397de67fe38e20db9d9f5bede80356f9870e63
naja moe <najamoe@outlook.dk>
2025-10-02 10:37:12 +0200

    db context added

---
+++ SubscriberService/Data/SubscriberServiceContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SubscriberService.Models;
+
+namespace SubscriberService.Data
+{
+    public class SubscriberServiceContext : DbContext
+    {
+        public SubscriberServiceContext (DbContextOptions<SubscriberServiceContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SubscriberService.Models.Subscriber> Subscriber { get; set; } = default!;
+    }
+}
