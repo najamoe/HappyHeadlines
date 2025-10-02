4534b6d33ba9ad74bc0be6c0fb031e450bcd749d
naja moe <najamoe@outlook.dk>
2025-10-02 10:36:57 +0200

    added post request for subscribers

---
+++ SubscriberService/Controllers/SubscribersController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SubscriberService.Data;
+using SubscriberService.Models;
+
+namespace SubscriberService.Controllers
+{
+    public class SubscribersController : Controller
+    {
+        private readonly SubscriberServiceContext _context;
+
+        public SubscribersController(SubscriberServiceContext context)
+        {
+            _context = context;
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] Subscriber subscriber)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(subscriber);
+                await _context.SaveChangesAsync();
+                return CreatedAtAction(nameof(Get), new { id = subscriber.Id }, subscriber);
+            }
+            return BadRequest(ModelState);
+        }
+    }
+}
